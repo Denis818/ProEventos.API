2624ccdb7cfe644ee4eb21f42cff1eee54fa8d96
Denis Developer <denispereiradosreis@gmail.com>
2023-06-04 15:08:21 -0300

    formart date

--- Domain/Dtos/EventDto.cs
+++ Domain/Dtos/EventDto.cs
@@ -1,12 +1,15 @@
-﻿namespace Domain.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Dtos
 {
     public class EventDto
     {
-        public string Local { get; set; }
+        [DataType(DataType.Date)]
         public DateTime DataEvento { get; set; }
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
         public string Lote { get; set; }
         public string ImagemURL { get; set; }
+        public string Local { get; set; }
     }
 }
