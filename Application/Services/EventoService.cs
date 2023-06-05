84d753bd6b59e1a359b0721b2b5c490458cb9968
Denis Developer <denispereiradosreis@gmail.com>
2023-06-04 23:09:09 -0300

    removendo try catch

--- Application/Services/EventoService.cs
+++ Application/Services/EventoService.cs
@@ -13,44 +13,32 @@ namespace Application.Services
 
         public async Task<Evento> InsertEvento(Evento evento)
         {
-            try
-            {
-                await _repository.InsertAsync(evento);
-                if (await _repository.SaveChangesAsync())
-                {
-                    return await _repository.GetAllEventosByIdAsync(evento.Id, false);
-                }
 
-                return null;
-            }
-            catch (Exception ex)
+            await _repository.InsertAsync(evento);
+            if (await _repository.SaveChangesAsync())
             {
-                throw new Exception(ex.Message);
+                return await _repository.GetAllEventosByIdAsync(evento.Id, false);
             }
+
+            return null;
         }
 
-        public async Task<Evento> UpdateEvento(int id, Evento model)
+        public async Task<Evento> UpdateEvento(int id, Evento modelEvento)
         {
-            try
+            var evento = await _repository.GetAllEventosByIdAsync(id, false);
+
+            if (evento == null) return null;
+
+            modelEvento.Id = evento.Id;
+
+            _repository.UpdateAsync(modelEvento);
+
+            if (await _repository.SaveChangesAsync())
             {
-                var evento = await _repository.GetAllEventosByIdAsync(id, false);
-                if (evento == null) return null;
-
-                model.Id = evento.Id;
-
-                _repository.UpdateAsync(model);
-
-                if (await _repository.SaveChangesAsync())
-                {
-                    return await _repository.GetAllEventosByIdAsync(model.Id, false);
-                }
-
-                return null;
+                return await _repository.GetAllEventosByIdAsync(modelEvento.Id, false);
             }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+
+            return null;
         }
 
         public Task<IEnumerable<Evento>> GetAllEventosAsync(string tema, bool includePalestrantes = false)
